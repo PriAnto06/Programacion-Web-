3c3d30e0a3d5eeb920bfd6330012ded39f611b7c
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-04-28 16:49:33 -0300

    vimos tema nuevo

---
+++ javascript/Actividad.js
@@ -0,0 +1,56 @@
+//1 if
+let numero = 19;
+if (numero > 10) {
+  console.log("El numero es mayor que 10");
+}
+
+//2 if
+let numero2 = 10;
+let numero3 = 16;
+if (numero2 > numero3) {
+  console.log("El primer numero es mayor", numero2);
+}
+if (numero3 > numero2) {
+  console.log("El segundo numero es mayor", numero3);
+} else {
+  console.log("ambos son iguales");
+}
+
+//1
+// while
+let number = 5;
+let counter = 0;
+while (counter <= number) {
+  counter++;
+  console.log(`el contador${counter}`);
+}
+//2
+//while
+let suma = 0;
+let numero4 = 1;
+while (numero4 <= 4)
+{
+    suma += numero4;
+    numero4++;
+    console.log(suma)
+} 
+
+//for
+//1
+
+for(let i = 0; i <=4; i++ )
+{
+  console.log(i)
+}
+
+//2
+ 
+let suma2 = 0;
+
+for (let i = 0; i <= 4; i++)
+{
+  suma2 += i;
+  console.log("La suma de los números del 0 al 4 es:", suma2);  
+}
+//console.log("La suma de los números del 0 al 4 es:", suma);
+
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,53 +2,56 @@
 //actualmente esta orientado a funciones
 
 //variables
-var miVariable = 'hola soy una viariable con var'//no se usa
-let miVariable2 = 'variable con let '//solo vive  en el scope declarado
-const miVariable3= 'constante con cambian los valores';
+var miVariable = "hola soy una viariable con var"; //no se usa
+let miVariable2 = "variable con let "; //solo vive  en el scope declarado
+const miVariable3 = "constante con cambian los valores";
 
-const hola = 'hola pri';
-console.log(hola)// es equivalente a print
+const hola = "hola pri";
+console.log(hola); // es equivalente a print
 //funciones
-function myfuction(variable)// la funcion tiene una declaracion
-  {
-   //en los parentesis van los parametros
-   console.log(variable)
-   return variable ; //en general las funciones retorna algo
-  }
+function myfuction(variable) {
+  // la funcion tiene una declaracion
+  //en los parentesis van los parametros
+  console.log(variable);
+  return variable; //en general las funciones retorna algo
+}
 //funcion flecha
 const myArrowFuction = (variable) => variable;
 
-//ejecucion de una funcion 
+//ejecucion de una funcion
 myfuction(8);
 
-const variable = true
+const variable = true;
 //condicional
-if(variable)
-{
-    console.log(true) 
-}  
-else
-{
- console.log(false) 
-}  
+if (variable) {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
 //condicional ternario sirve solo para dos clasulas
 variable ? console.log(true) : console.log(false);
 
 //arreglos o arrays
 //los arreglos son las litas de python
-const arr = [1,true, "hola como estas", 0,1,2]
+const arr = [1, true, "hola como estas", 0, 1, 2];
 
 console.log(`la posicion 1 del arreglo es,${arr[1]}`);
 
-//objetos 
+//objetos
 //los objetos son como los diccionarios en python
 const objeto = {
-    clave : "valor",
+  clave: "valor",
 };
 //los objetos estan compuestos por clave valor
 //accedemos al valor de la clave por medio del punto
-console.log(objeto.clave)
+console.log(objeto.clave);
+
+//bucle for
+const arrayNumeros = [1, 2, 3, "Hola"];
+for (let i = 0; i < arrayNumeros.length; i++) 
+{
+  console.log(arrayNumeros[i]);
+}
 
 
- 
