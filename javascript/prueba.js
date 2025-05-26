aae874d60d70100d5816c923d9239ce5cc56c81e
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-05-26 16:44:55 -0300

    mucho no anote pero aanote lo mas relevante

---
+++ javascript/prueba.js
@@ -0,0 +1,12 @@
+var var1 = 2;//vive en cualquier scopee,reasignable sus valores
+let redefinible = 1; //vive donde es declarado y en sus hijos, reasignable sus valores  
+
+//for flexible
+//forEach
+//ambos recorren
+
+//map y filter devuelven nueva lista
+
+const estudiante = {nombre:"Pri"}//laves: objeto
+                //[] lista
+
