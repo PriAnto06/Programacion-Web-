4b6624801034eefdf1ae68dd6dd540ca3c440042
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-06-02 15:51:46 -0300

    Continue con ejercecios

---
+++ javascript/asincronia/actividad.js
@@ -0,0 +1,15 @@
+//Actividad 1
+
+const contraseña = "Hola"
+const usuario = "Pri"
+
+function addLogin(){
+    const input = document.getElementById("User")
+    const text = input.value;
+    console.log(text)
+}
+const value = "Pri"
+localStorage.setItem("Nombre")
+
+
+
