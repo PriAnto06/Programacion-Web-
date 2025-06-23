6c1618e51ca4fcb38a12b6765bcf2d1d5906ead5
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-06-23 16:51:39 -0300

    Tema nuevo

---
+++ React-Teoria-Priscila Della Torre/src/components/provider/provider.jsx
@@ -0,0 +1,3 @@
+export function Provider({name,apellido}){
+    return <>{apellido}</>;
+}
---
+++ React-Teoria-Priscila Della Torre/src/components/user/user.jsx
@@ -0,0 +1,12 @@
+import{Provider} from"../provider/Provider";
+//las props  son datos/funciones que podemos 
+//pasar de un componente a otro
+export function User(){
+    return (
+    <>
+    hola
+    <Provider name ="Pri" apellido="Della Torre"/>
+    </>
+);
+}
+//componentes se hace asi
